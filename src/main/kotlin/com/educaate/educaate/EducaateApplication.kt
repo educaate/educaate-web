4e93be677811f1a83a90901a09ca19b622f4ab24
Amin Moradian <amin.moradian@protonmail.com>
2019-05-23 12:12:49 +0430

    initial commit.

---
+++ src/main/kotlin/com/educaate/educaate/EducaateApplication.kt
@@ -0,0 +1,11 @@
+package com.educaate.educaate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EducaateApplication
+
+fun main(args: Array<String>) {
+	runApplication<EducaateApplication>(*args)
+}
